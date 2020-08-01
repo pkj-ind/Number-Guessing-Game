a18af4eb85ef470c30983e579d0c6624ed449122
pkj-ind <pramodix@gmail.com>
2020-08-01 08:59:41 +0530

    conditional touchable effect for differnt platform

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback, Platform, TouchableNativeFeedback } from "react-native";
 import Colors from "../Constants/Colors"
 
 const MainButton = (props) => {
+  let ButtonContainer = TouchableOpacity;
+  //TouchanbleNativeFeedback gives ripple effect
+  ButtonContainer = Platform.OS === "android" && Platform.Version > 21 && TouchableNativeFeedback;
   return (
-    <TouchableOpacity onPress={props.onPress} activeOpacity={0.6}>
+    <View style={styles.buttonContainer}>
+    <ButtonContainer onPress={props.onPress} activeOpacity={0.6}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{props.children}</Text>
       </View>
-    </TouchableOpacity>
+    </ButtonContainer>
+    </View>
   );
 };
 
 export default MainButton;
 
 const styles = StyleSheet.create({
+  buttonContainer:{
+   borderRadius:25,
+   overflow:"hidden" // rippleeffects is align with button now, its overflowing is hidden from user now.
+  },
   button: {
       backgroundColor: Colors.primary,
       paddingVertical: 12,
