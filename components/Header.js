96186c10875fe814c6f46b07a56da1f7faa9d548
pkj-ind <pramodix@gmail.com>
2020-08-01 07:51:17 +0530

    conditional styling with platform.select()

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View, Platform } from 'react-native'
-import Colors from "../Constants/Colors"
+import React from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import Colors from "../Constants/Colors";
 
 const Header = (props) => {
-    return (
-        <View style={styles.header}>
-            <Text style={styles.title} >{props.title}</Text>
-        </View>
-    )
-}
+  return (
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({ ios: styles.headerIOS, android: styles.headerAndroid }),
+      }}
+    >
+      <Text style={styles.title}>{props.title}</Text>
+    </View>
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-header:{
-    width:'100%',
-    height:90,
-    paddingTop:36,
-    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
-    alignItems:"center",
-    justifyContent:"center",
-    borderBottomWidth:  Platform.OS === "android" ? 0 : 1,
-    borderBottomColor: Platform.OS === "android" ? "transparent" : "#ccc"
-},
-title:{
+  headerBase: {
+    width: "100%",
+    height: 90,
+    paddingTop: 36,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerIOS: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    backgroundColor: "white",
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+  },
+  title: {
     color: Platform.OS === "ios" ? Colors.primary : "white",
-    fontSize:18,
-}
-})
+    fontSize: 18,
+  },
+});
